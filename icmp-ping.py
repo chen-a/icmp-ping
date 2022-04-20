a821a0e83ee99d660a43751e2745ba559cdf2eaa
chen-a <andyac2909@gmail.com>
2022-04-20 01:07:00 -0500

    checksum

---
+++ icmp-ping.py
@@ -0,0 +1,25 @@
+import socket
+import sys
+import os
+
+def checksum(string):
+    csum = 0
+    countTo = (len(string) // 2) * 2
+    count = 0
+
+    while count < countTo:
+        thisVal = ord(string[count+1]) * 256 + ord(string[count])
+        csum = csum + thisVal
+        csum = csum & 0xffffffff
+        count = count + 2
+
+    if countTo < len(string):
+        csum = csum + ord(string[len(string) - 1])
+        csum = csum & 0xffffffff
+
+    csum = (csum >> 16) + (csum & 0xffff)
+    csum = csum + (csum >> 16)
+    answer = ~csum
+    answer = answer & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+    return answer 
